5917eb9e1ef3cd5c20f587874c7c949b5a3b5b2f
lukaskierzek <lukas.kierzek@gmail.com>
2022-06-17 15:29:48 +0200

    Create new classes
    Add all classes from models to admin

--- road_signs_repair/admin.py
+++ road_signs_repair/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Locality, RoadSign, RoadSignsForRepair, Region
+
 
 # Register your models here.
+# Login: Admin
+# Password: zaq1@WSX
+
+
+@admin.register(Locality)
+class LocalityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RoadSign)
+class RoadSignAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RoadSignsForRepair)
+class RoadSignsForRepairAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    pass
