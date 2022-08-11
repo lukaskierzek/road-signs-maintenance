3a92eda48157ee9e80272716ddb49f51f713764e
Kierzu <kierzu@LENOVO-KIERZU.localdomain>
2022-08-11 21:16:17 +0200

    Add new classes
    Add new classes to display information

--- road_signs_repair/admin.py
+++ road_signs_repair/admin.py
@@ -6,22 +6,95 @@ from .models import Locality, RoadSign, RoadSignsForRepair, Region
 # Login: Admin
 # Password: zaq1@WSX
 
+class LocalityInline(admin.TabularInline):
+    model = Locality
+    extra = 0
+    fields = [
+        "name",
+        "region",
+    ]
+
+
+class RoadSignsForRepairInline(admin.TabularInline):
+    model = RoadSignsForRepair
+    extra = 0
+    fields = [
+        "region",
+        "locality",
+        "sign",
+        "description_of_the_place",
+        "task",
+        "report_date",
+        "repair_date",
+    ]
+
 
 @admin.register(Locality)
 class LocalityAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("__str__",
+                    "region",)
+
+    list_filter = ("region",)
+
+    search_fields = [
+        "name",
+        "region__name",
+    ]
+
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Region', {'fields': ['region']}),
+    ]
+
+    inlines = [RoadSignsForRepairInline]
 
 
 @admin.register(RoadSign)
 class RoadSignAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("__str__",
+                    "name",
+                    "description")
+
+    list_filter = ("series",)
+
+    search_fields = [
+        "series",
+        "name",
+        "description",
+    ]
+
+    inlines = [RoadSignsForRepairInline]
 
 
 @admin.register(RoadSignsForRepair)
 class RoadSignsForRepairAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("__str__",
+                    "region",
+                    "locality",
+                    "sign",
+                    "description_of_the_place",
+                    "task",
+                    "report_date",
+                    "repair_date",)
+
+    list_filter = ("region",
+                   "locality",
+                   "sign",
+                   "task",
+                   "report_date",
+                   "repair_date",)
+
+    search_fields = [
+        "description_of_the_place",
+        "region",
+        "sign",
+    ]
 
 
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ("name",)
+
+    search_fields = ["name", ]
+
+    inlines = [LocalityInline, RoadSignsForRepairInline]
