a122837e153dc605dce342ea382d20a8844d626d
lukaskierzek <lukas.kierzek@gmail.com>
2022-08-14 14:18:33 +0200

    Add new path Remove extra space

--- road_signs_maintenance/urls.py
+++ road_signs_maintenance/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    # path('road_signs_repair/', include(road_signs_repair.urls)),
     path('admin/', admin.site.urls),
+    path('road_signs_repair/', include('road_signs_repair.urls')),
+    path('', RedirectView.as_view(url='road_signs_repair/'))
 ]
