38bbf3282e5a62b9c662d37e9d1b912eb95a0529
lukaskierzek <lukas.kierzek@gmail.com>
2022-08-14 14:24:28 +0200

    Create a new classes

--- road_signs_repair/views.py
+++ road_signs_repair/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import RoadSignsForRepair, Region
+
 
 # Create your views here.
+class RoadSignsForRepairListView(generic.ListView):
+    model = RoadSignsForRepair
+    context_object_name = 'road_signs_for_repair'
+    template_name = 'road_signs_repair/index.html'
+
+
+class RegionDetailView(generic.DetailView):
+    model = Region
+    context_object_name = 'region'
+    template_name = 'road_signs_repair/region_detail.html'
