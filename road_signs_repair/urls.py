2a411eef9daa4fe58dd034ea42d2f5ee0caaa0ca
lukaskierzek <lukas.kierzek@gmail.com>
2022-08-14 14:23:48 +0200

    Create new path and app_name

---
+++ road_signs_repair/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'road_signs_repair'
+
+urlpatterns = [
+    path('', views.RoadSignsForRepairListView.as_view(), name='road_signs_for_repair'),
+    path('region/<int:pk>/', views.RegionDetailView.as_view(), name='region')
+]
