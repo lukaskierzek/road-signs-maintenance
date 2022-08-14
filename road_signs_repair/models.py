bd41e2f1523c966312f2f7087d639260269b7a29
lukaskierzek <lukas.kierzek@gmail.com>
2022-08-14 14:22:13 +0200

    Add new function in the Region class

--- road_signs_repair/models.py
+++ road_signs_repair/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -10,6 +11,11 @@ class Region(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('road_signs_repair:region', kwargs={
+            'pk': self.id,
+        })
 
     class Meta:
         permissions = ()
