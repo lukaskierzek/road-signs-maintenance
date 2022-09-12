88ec0f7c2b624d9771edd75736affdabc3e5be12
lukaskierzek <lukas.kierzek@gmail.com>
2022-09-12 21:52:21 +0200

    Add LoginRequiredMixin to all classes

--- road_signs_repair/views.py
+++ road_signs_repair/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import RoadSignsForRepair, Region
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class RoadSignsForRepairListView(generic.ListView):
+class RoadSignsForRepairListView(LoginRequiredMixin, generic.ListView):
     model = RoadSignsForRepair
     context_object_name = 'road_signs_for_repair'
     template_name = 'road_signs_repair/index.html'
 
 
-class RoadSignsForRepairDetailView(generic.DetailView):
+class RoadSignsForRepairDetailView(LoginRequiredMixin, generic.DetailView):
     model = RoadSignsForRepair
     context_object_name = 'road_signs_for_repair_detail'
     template_name = 'road_signs_repair/road_signs_for_repair_detail.html'
 
 
-class RegionDetailView(generic.DetailView):
+class RegionDetailView(LoginRequiredMixin, generic.DetailView):
     model = Region
     context_object_name = 'region'
     template_name = 'road_signs_repair/region_detail.html'
