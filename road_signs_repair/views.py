2298b598463a0f3cc506d25f70b4c076bce99eb7
lukaskierzek <lukas.kierzek@gmail.com>
2022-09-24 22:48:22 +0200

    Modify views.py
    Remove permission_required, PermissionRequiredMixin
     from RoadSignsForRepairDetailView class
    
    Add RoadSignsForRepairUpdate class to update task

--- road_signs_repair/views.py
+++ road_signs_repair/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
 from django.views import generic
 from .models import RoadSignsForRepair, Region
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -11,11 +12,27 @@ class RoadSignsForRepairListView(LoginRequiredMixin, generic.ListView):
     template_name = 'road_signs_repair/index.html'
 
 
-class RoadSignsForRepairDetailView(LoginRequiredMixin, PermissionRequiredMixin, generic.DetailView):
+class RoadSignsForRepairDetailView(LoginRequiredMixin, generic.DetailView):
     model = RoadSignsForRepair
     context_object_name = 'road_signs_for_repair_detail'
     template_name = 'road_signs_repair/road_signs_for_repair_detail.html'
-    permission_required = ('road_signs_repair.can_update_task',)
+
+
+class RoadSignsForRepairUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = RoadSignsForRepair
+    fields = [
+        'region',
+        'locality',
+        'sign',
+        'description_of_the_place',
+        'task',
+        'mode',
+        'report_date',
+        'repair_date',
+    ]
+    template_name = 'road_signs_repair/road_signs_for_repair_form.html'
+    permission_required = ('road_signs_repair.can_update_task', )
+    context_object_name = 'road_signs_for_repair_update'
 
 
 class RegionDetailView(LoginRequiredMixin, generic.DetailView):
