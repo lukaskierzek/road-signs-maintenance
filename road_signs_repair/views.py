a6369a6ec65f63c8391dc57b3392737db659a342
lukaskierzek <lukas.kierzek@gmail.com>
2022-11-18 20:59:43 +0100

    Modify road_sings_repair.views.py file
    Reorganize imports and remove whitespace

--- road_signs_repair/views.py
+++ road_signs_repair/views.py
@@ -1,18 +1,17 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import UpdateView, DeleteView
-from django.views import generic
+from django.views.generic.edit import UpdateView
+from django.views.generic import ListView, DetailView
 from .models import RoadSignsForRepair, Region
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 # Create your views here.
-class RoadSignsForRepairListView(LoginRequiredMixin, generic.ListView):
+class RoadSignsForRepairListView(LoginRequiredMixin, ListView):
     model = RoadSignsForRepair
     context_object_name = 'road_signs_for_repair'
     template_name = 'road_signs_repair/index.html'
 
 
-class RoadSignsForRepairDetailView(LoginRequiredMixin, generic.DetailView):
+class RoadSignsForRepairDetailView(LoginRequiredMixin, DetailView):
     model = RoadSignsForRepair
     context_object_name = 'road_signs_for_repair_detail'
     template_name = 'road_signs_repair/road_signs_for_repair_detail.html'
@@ -31,11 +30,11 @@ class RoadSignsForRepairUpdate(LoginRequiredMixin, PermissionRequiredMixin, Upda
         'repair_date',
     ]
     template_name = 'road_signs_repair/road_signs_for_repair_form.html'
-    permission_required = ('road_signs_repair.can_update_task', )
+    permission_required = ('road_signs_repair.can_update_task',)
     context_object_name = 'road_signs_for_repair_update'
 
 
-class RegionDetailView(LoginRequiredMixin, generic.DetailView):
+class RegionDetailView(LoginRequiredMixin, DetailView):
     model = Region
     context_object_name = 'region'
     template_name = 'road_signs_repair/region_detail.html'
