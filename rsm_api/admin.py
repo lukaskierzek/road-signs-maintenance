2a0aa8d8917086e248166ecad421ee475bae9268
lukaskierzek <lukas.kierzek@gmail.com>
2022-11-21 15:14:22 +0100

    Modify admin.py
    Create admins classes for models

--- rsm_api/admin.py
+++ rsm_api/admin.py
@@ -1,3 +1,106 @@
 from django.contrib import admin
+from .models import Locality, RoadSign, RoadSignsForRepair, Region
+
 
 # Register your models here.
+# Login: admin
+# Password: admin
+
+class LocalityInline(admin.TabularInline):
+    model = Locality
+    extra = 0
+    fields = [
+        "name",
+        "region",
+    ]
+
+
+class RoadSignsForRepairInline(admin.TabularInline):
+    model = RoadSignsForRepair
+    extra = 0
+    fields = [
+        "region",
+        "locality",
+        "sign",
+        "description_of_the_place",
+        "task",
+        "mode",
+        "report_date",
+        "repair_date",
+    ]
+
+
+@admin.register(Locality)
+class LocalityAdmin(admin.ModelAdmin):
+    list_display = ("__str__",
+                    "region",)
+
+    list_filter = ("region",)
+
+    search_fields = [
+        "name",
+        "region__name",
+    ]
+
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Region', {'fields': ['region']}),
+    ]
+
+    inlines = [RoadSignsForRepairInline]
+
+
+@admin.register(RoadSign)
+class RoadSignAdmin(admin.ModelAdmin):
+    list_display = ("__str__",
+                    "name",
+                    "description",)
+
+    list_filter = ("series",)
+
+    search_fields = [
+        "series",
+        "name",
+        "description",
+    ]
+
+    inlines = [RoadSignsForRepairInline]
+
+
+@admin.register(RoadSignsForRepair)
+class RoadSignsForRepairAdmin(admin.ModelAdmin):
+    list_display = ("__str__",
+                    "region",
+                    "locality",
+                    "sign",
+                    "description_of_the_place",
+                    "task",
+                    "mode",
+                    "report_date",
+                    "repair_date",)
+
+    list_filter = ("region",
+                   "locality",
+                   "sign",
+                   "task",
+                   "mode",
+                   "report_date",
+                   "repair_date",)
+
+    search_fields = [
+        "description_of_the_place",
+        "region",
+        "sign",
+        "mode",
+    ]
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("__str__",)
+
+    list_filter = ("name",)
+
+    search_fields = ["name", ]
+
+    inlines = [LocalityInline, RoadSignsForRepairInline]
