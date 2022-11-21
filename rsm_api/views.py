3691ae49d5d76092ab218482f13c37baa0e5221f
lukaskierzek <lukas.kierzek@gmail.com>
2022-11-21 15:31:32 +0100

    Modify rsm_api.views.py
    Add classes from serializer to display models

--- rsm_api/views.py
+++ rsm_api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
-
+from rest_framework import generics
 # Create your views here.
+from .serializer import RoadSignsForRepairSerializer, RoadSignSerializer, LocalitySerializer, RegionSerializer
+from .models import RoadSignsForRepair, Region, RoadSign, Locality
+
+
+class RoadSignsForRepairList(generics.ListCreateAPIView):
+    serializer_class = RoadSignsForRepairSerializer
+    queryset = RoadSignsForRepair.objects.all()
+
+
+class RoadSignsForRepairDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RoadSignsForRepairSerializer
+    queryset = RoadSignsForRepair.objects.all()
+
+
+class RoadSignList(generics.ListCreateAPIView):
+    serializer_class = RoadSignSerializer
+    queryset = RoadSign.objects.all()
+
+
+class RoadSignDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RoadSignSerializer
+    queryset = RoadSign.objects.all()
+
+
+class LocalityList(generics.ListCreateAPIView):
+    serializer_class = LocalitySerializer
+    queryset = Locality.objects.all()
+
+
+class LocalityDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LocalitySerializer
+    queryset = Locality.objects.all()
+
+
+class RegionList(generics.ListCreateAPIView):
+    serializer_class = RegionSerializer
+    queryset = Region.objects.all()
+
+
+class RegionDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RegionSerializer
+    queryset = Region.objects.all()
